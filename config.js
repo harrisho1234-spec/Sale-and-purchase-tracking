a75641297e167e6d0b1ded2c7175326ccc5d2b11
harrisho1234-spec <harris.ho1234@gmail.com>
2026-09-22 12:39:01 +0700

    Connect Google Drive product photo uploader

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
   SUPABASE_URL: "https://msxvnaintafqdgheutfu.supabase.co",
   SUPABASE_PUBLISHABLE_KEY: "sb_publishable_RpLdBcXql9MpBZCU-GLqPQ_G3VKM8zd",
-  DRIVE_PHOTO_UPLOAD_URL: "",
+  DRIVE_PHOTO_UPLOAD_URL: "https://script.google.com/macros/s/AKfycbzsjMFu8NVYcOBVY8jA0avorVUzFKQLmJsvEqEffqmcbIDt0dNHY0I6D4vnGbZFrC7Dbw/exec",
   APP_NAME: "L'Imperial Sales & Order Management"
 };
